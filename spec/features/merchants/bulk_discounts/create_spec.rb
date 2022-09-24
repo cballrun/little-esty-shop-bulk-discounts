8dac5902480cec1a16bf56b47ba82e7135fe1277
Carter Ball <carter.k.ball@gmail.com>
2022-09-23 23:24:55 -0400

    Bulk Discount create working as expected

--- spec/features/merchants/bulk_discounts/create_spec.rb
+++ spec/features/merchants/bulk_discounts/create_spec.rb
@@ -21,7 +21,22 @@ RSpec.describe 'Merchant Bulk Discount New' do
         fill_in 'Quantity', with: 20
        
         expect { click_on 'Create Bulk discount' }.to change { BulkDiscount.count }.by(1)
+        expect(current_path).to eq(merchant_bulk_discounts_path(@merchant))
       end
+    end
+
+    context 'given invalid data' do
+      it 'redirects to the new bulk discount page with instructions on how to fix the errors' do
+        fill_in 'Percentage', with: 200
+        fill_in 'Quantity', with: -200
+
+        click_on 'Create Bulk discount'
+
+        expect(current_path).to eq(new_merchant_bulk_discount_path(@merchant))
+        expect(page).to have_content("Percentage must be less than or equal to 100")
+        expect(page).to have_content("Quantity must be greater than or equal to 0")
+      end
+
     end
   end
 
