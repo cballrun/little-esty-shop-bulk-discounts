7d06b02bba780271dde15404e84562fcfcfe8323
Carter Ball <carter.k.ball@gmail.com>
2022-09-23 21:20:11 -0400

    Link to Merchant Bulk Discounts index on Merchant dashboard working

--- spec/features/merchants/bulk_discounts/index_spec.rb
+++ spec/features/merchants/bulk_discounts/index_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'the Merchant Bulk Discounts Index' do
     @merchants = create_list(:merchant, 3)
     @bulk_discounts_0 = create_list(:bulk_discount, 3, merchant: @merchants[0])
     @bulk_discounts_1 = create_list(:bulk_discount, 4, merchant: @merchants[1])
-    visit "/merchants/#{@merchants[0].id}/bulk_discounts"
+    visit merchant_bulk_discounts_path(@merchants[0])
   end
 
   describe 'as a merchant' do
