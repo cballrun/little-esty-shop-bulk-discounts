ae13c76b6eaca25c7749c23c568698daa76c9edf
Carter Ball <carter.k.ball@gmail.com>
2022-09-23 22:30:05 -0400

    Bulk Discount Model verification updates

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,5 +1,7 @@
 class BulkDiscount < ApplicationRecord
   belongs_to :merchant
   validates_presence_of :percentage, :quantity
+  validates_numericality_of :percentage, only_integer: true, greater_than: 0, less_than_or_equal_to: 100
+  validates_numericality_of :quantity, only_integer: true, greater_than_or_equal_to: 0
 
 end
--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe BulkDiscount, type: :model do
   describe 'validations' do
     it { should validate_presence_of :percentage }
     it { should validate_presence_of :quantity }
+    it { should validate_numericality_of :percentage }
+    it { should validate_numericality_of :quantity }
   end
 
   describe 'relationships' do
